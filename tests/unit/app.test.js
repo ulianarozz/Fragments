f48bdb6b2c101070baadeba50a399eef85df1dba
Uliana <ulianarozz@gmail.com>
2022-12-12 23:28:31 -0500

    clear

--- tests/unit/app.test.js
+++
@@ -1,7 +0,0 @@
-const request = require('supertest');
-
-const app = require('../../src/app');
-
-describe('Not Found', () => {
-  test('Not Found', () => request(app).get('/not-found').expect(404));
-});
